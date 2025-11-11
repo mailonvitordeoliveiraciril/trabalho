c6b7b050c273832ebbb0a6b5dedc15adb143af90
mailon <mailon.ciril@escola.pr.gov.br>
2025-11-11 12:15:14 +0000

    concluido

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-body{
-    text-align: center;
-    font-family: Arial;
-    
-    
+function Dinossauro(){
+    document.getElementById("Dinossauro 2").src ="on.png"
+    console.log("On")
+}
+
+function Dinossauro(){
+    document.getElementById("Dinossauro").src ="off.png"
+    console.log("off")
 }
